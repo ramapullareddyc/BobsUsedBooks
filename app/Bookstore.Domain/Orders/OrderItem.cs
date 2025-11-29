0929fc3725278db47369801667a350aeea154db6
NoReply Amazon <no-reply@amazon.com>
2025-11-29 21:14:28 +0000

    DB transformation: Upload transformed code

--- app/Bookstore.Domain/Orders/OrderItem.cs
+++ app/Bookstore.Domain/Orders/OrderItem.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Bookstore.Domain.Books;
 
 namespace Bookstore.Domain.Orders
 {
-    [Table("order_item", Schema = "dbo")]
+    [Table("OrderItem", Schema = "dbo")]
     public class OrderItem : Entity
     {
         // This private constructor is required by EF Core
@@ -18,15 +19,15 @@ namespace Bookstore.Domain.Orders
             Quantity = quantity;
         }
 
-        [Column("order_id")]
+        [Column("OrderId")]
         public int OrderId { get; set; }
         public Order Order { get; set; }
 
-        [Column("book_id")]
+        [Column("BookId")]
         public int BookId { get; set; }
         public Book Book { get; set; }
 
-        [Column("quantity")]
+        [Column("Quantity")]
         public int Quantity { get; set; }
     }
 }
