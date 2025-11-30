82638fc33c54b5bc91cc0c3ee5ba45b58f7c6e58
NoReply Amazon <no-reply@amazon.com>
2025-11-30 01:04:30 +0000

    DB transformation: Upload transformed code

--- app/Bookstore.Web/Startup/ServicesSetup.cs
+++ app/Bookstore.Web/Startup/ServicesSetup.cs
@@ -14,6 +14,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Npgsql;
 
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 
 namespace Bookstore.Web.Startup
 {
