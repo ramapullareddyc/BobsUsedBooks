723b7a5da807c5d00b7fe53030c92c23fa84704a
NoReply Amazon <no-reply@amazon.com>
2025-11-30 12:15:42 +0000

    DB transformation: Upload transformed code

--- app/Bookstore.Web/Startup/ServicesSetup.cs
+++ app/Bookstore.Web/Startup/ServicesSetup.cs
@@ -12,8 +12,6 @@ using System.Text.Json;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
-
 using Npgsql;
 
 
@@ -91,11 +89,11 @@ namespace Bookstore.Web.Startup
 //                    PropertyNameCaseInsensitive = true
 //                });
 //
-//                var partialConnString = $"Server={dbSecrets.Host},{dbSecrets.Port}; Initial Catalog=BobsUsedBookStore;MultipleActiveResultSets=true; Integrated Security=false";
+//                var partialConnString = $"Host={dbSecrets.Host};Port={dbSecrets.Port};Database=postgres";
 //
 //                var builder = new NpgsqlConnectionStringBuilder(partialConnString)
 //                {
-//                    UserID = dbSecrets.Username,
+//                    Username = dbSecrets.Username,
 //                    Password = dbSecrets.Password
 //                };
 //
